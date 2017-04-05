499c61ffeba4330c8e92a630e6b5bfdf3197f47c
Robbert van Markus <robbert@vanmarkus.com>
2017-04-05 21:25:36 +0200

    routing + theming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import {AppComponent} from "./app.component";
 import {AluecoOrderFormComponent} from "./order/alueco-order-form.component";
 import {OrderOverviewComponent} from "./order/order-overview.component";
 export const appRoutes: Routes = [
-  { path: '', component: OrderOverviewComponent },
-  { path: 'order/:id',      component: AluecoOrderFormComponent },
   {
-    path: 'orders',
-    component: OrderOverviewComponent,
+    path: '',
+    component: OrderOverviewComponent
+  },
+  {
+    path: 'containers/overzicht',
+    component: OrderOverviewComponent
+  },
+  {
+    path: 'containers/bestellen',
+    component: AluecoOrderFormComponent,
   },
   // { path: '**', component: PageNotFoundComponent }
 ];
