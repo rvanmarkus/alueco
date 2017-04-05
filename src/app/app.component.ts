2f8b86c84a7392afb7e617e0f39b0951dea991c5
Robbert van Markus <robbert@vanmarkus.com>
2017-04-05 20:48:53 +0200

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 @Component({
   selector   : 'app',
   templateUrl: './app.component.html',
-  styleUrls: ['./material-theme.scss'],
+  styleUrls: ['./material-theme.scss', './app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
